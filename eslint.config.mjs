1bf07534c316409824c7f5ae488b2a9bcd62bf12
seohyoseung <djmonnar4@gmail.com>
2026-05-12 01:08:19 +0900

    Publish static export at repository root

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,8 @@ const eslintConfig = defineConfig([
     // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
+    "_next/**",
+    "_not-found/**",
     "build/**",
     "next-env.d.ts",
   ]),
